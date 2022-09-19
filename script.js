52dd00ed90ce2f12b3f4377ad0193400416ef4a3
islamhack01 <iaubakirov503@gmail.com>
2022-09-19 15:57:41 +0600

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+// let openBtn = document.querySelector('.burger')
+// let closeBtn = document.querySelector('.burger_close')
+// let burgerMenu = document.querySelector('.burger-menu')
+
+// openBtn.addEventListener('click', () => {
+//     burgerMenu.style.display = 'block'
+// });
+
+// closeBtn.addEventListener('click', () => {
+//     burgerMenu.style.display = 'none'
+// })
